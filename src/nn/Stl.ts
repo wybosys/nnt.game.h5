9b02576e2d7e733bb08afb593927943d7271bc00
wybosys <wybosys@gmail.com>
2017-01-25 17:28:11 +0800

    stl

--- src/nn/Stl.ts
+++ src/nn/Stl.ts
@@ -1,5 +1,8 @@
 module nn {
 
+    // 提供一个类型，用来声明利用Object来模拟Map的类型
+    export class KvObject <K, V> {}
+
     declare var Map;
     declare var Set;
     var ECMA6_NATIVE:boolean = true;
@@ -95,5 +98,99 @@ module nn {
         get:(k:K)=>V = ECMA6_NATIVE ? this._n_get : this._i_get;        
     }
 
+
+    export class CSet <V> {
+
+        constructor() {
+            if (ECMA6_NATIVE) {
+                this._imp = new Set();
+            } else {
+                this._map = new CMap<any, V>();
+                this._arr = new Array<V>();
+            }
+        }
+
+        private _imp:any;
+        private _map:CMap<any, V>;
+        private _arr:Array<V>;
+
+        private _n_add(o:V):boolean {
+            return this._imp.add(o);
+        }
+
+        private _i_add(o:V):boolean {
+            var k = Js.hashKey(o);
+            if (this._map[k] != undefined)
+                return false;
+            this._map[k] = true;
+            this._arr.push(o);
+            return true;
+        }
+
+        add:(o:V)=>boolean = ECMA6_NATIVE ? this._n_add : this._i_add;
+
+        private _n_has(o:V):boolean {
+            return this._imp.has(o);
+        }
+
+        private _i_has(o:V):boolean {
+            var k = Js.hashKey(o);
+            return this._map[k] != undefined;       
+        }
+
+        has:(o:V)=>boolean = ECMA6_NATIVE ? this._n_has : this._i_has;
+
+        private _n_delete(o:V):boolean {
+            return this._imp.delete(o);
+        }
+
+        private _i_delete(o:V):boolean {
+            var k = Js.hashKey(o);
+            if (this._map[k] == undefined)
+                return false;
+            this._map.delete(k);
+            var idx = this._arr.indexOf(o);
+            this._arr.splice(idx, 1);
+            return true;
+        }
+
+        delete:(o:V)=>boolean = ECMA6_NATIVE ? this._n_delete : this._i_delete;
+
+        private _n_size():number {
+            return this._imp.size;
+        }
+
+        private _i_size():number {
+            return this._arr.length;
+        }
+        
+        get size():number {
+            return ECMA6_NATIVE ? this._n_size() : this._i_size();
+        }
+
+        private _n_clear() {
+            this._imp.clear();
+        }
+
+        private _i_clear() {
+            if (this._arr.length) {
+                this._map.clear();
+                this._arr.length = 0;
+            }
+        }
+
+        clear:()=>void = ECMA6_NATIVE ? this._n_clear : this._i_clear;
+
+        private _n_foreach(cb:(o:V)=>void, ctx?:any) {
+            this._imp.forEach(cb, ctx);
+        }
+
+        private _i_foreach(cb:(o:V)=>void, ctx?:any) {
+            if (this._arr.length)
+                this._arr.forEach(cb, ctx);
+        }
+
+        forEach:(cb:(o:V)=>void, ctx?:any)=>void = ECMA6_NATIVE ? this._n_foreach : this._i_foreach;
+    }
 
 }
