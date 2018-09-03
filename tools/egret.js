3c8a7c3b55f13147e9140bd46fc5169b3703fbf4
wybosys <wybosys@gmail.com>
2018-09-03 17:38:22 +0800

    prj

--- tools/egret.js
+++ tools/egret.js
@@ -1,13 +1,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const game_1 = require("./game");
+const fs = require("fs");
+const del = require("del");
 class EgretGame extends game_1.Game {
     clean() {
         super.clean();
-        /*
         // 清除egret的中间文件
         if (fs.existsSync("bin-debug"))
-            del.sync("bin-debug/**");
-            */
+            del.sync("bin-debug");
     }
 }
 exports.EgretGame = EgretGame;
