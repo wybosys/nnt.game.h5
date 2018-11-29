4fa61c6f0cc142c106946ff93bad155a3f0ee4fe
wybosys <wybosys@gmail.com>
2018-11-29 16:02:33 +0800

    prj

--- project/src/app/main/MainScene.ts
+++ project/src/app/main/MainScene.ts
@@ -1,20 +1,38 @@
 module app.main {
     interface IMainScene
-    {
+        extends nn.ILauncher {
         //slot {
         //slot }
     }
 
     export class MainScene
-    extends eui.SpriteU
-    implements IMainScene
-    {
+        extends eui.SpriteU
+        implements IMainScene {
         //skin {
         viewStack: eui.NavigationU;
+
         //skin }
 
         onLoaded() {
             this.viewStack.push(new Sample());
+        }
+
+        launchEntry(cls: any, data: nn.EntrySettings) {
+            // 如果已经打开，则不处理
+            if (this.viewStack.topView() instanceof cls)
+                return;
+
+            // 打开新页面
+            let p = new cls();
+            p.entrySettings = data;
+
+            if (p instanceof eui.DialogU) {
+                let dlg = <eui.DialogU>p;
+                dlg.open(false);
+            } else {
+                this.viewStack.pop();
+                this.viewStack.push(p);
+            }
         }
     }
 }
--- project/src/app/wgt/EntryButton.ts
+++ project/src/app/wgt/EntryButton.ts
@@ -12,12 +12,34 @@ module app.wgt {
         iconDisplay: eui.ImageU;
         labelDisplay: eui.LabelU;
         spBkg: eui.RectU;
+
         //skin }
 
         onLoaded() {
             let br = new nn.Brush();
             br.color = nn.Color.Random();
             this.spBkg.brush = br;
+
+            this.signals.connect(nn.SignalClicked, this._actOpenEntry, this);
+        }
+
+        // 实体标记，用来自动打开
+        _dentry: data.Entry;
+
+        public get entryIdr(): string {
+            return this._dentry ? this._dentry.entryIdr : null;
+        }
+
+        public set entryIdr(v: string) {
+            this._dentry = data.Entry.FromIdr(v);
+        }
+
+        private _actOpenEntry() {
+            if (!this._dentry)
+                return;
+
+            // 跳转到模块
+            this._dentry.open();
         }
     }
 }
