92f7f3162ce0361c0e462f08079719fe7f2bd846
wybosys <wybosys@gmail.com>
2018-11-29 18:53:09 +0800

    guide

--- project/src/app/guide/Shou.ts
+++ project/src/app/guide/Shou.ts
@@ -8,6 +8,11 @@ module app.guide {
         extends eui.DialogU
         implements IShou {
         //skin {
+        idrBottom: eui.LabelU;
+        idrLeft: eui.LabelU;
+        idrRight: eui.LabelU;
+        idrTop: eui.LabelU;
+        mcShou: eui.MovieClipU;
         //skin }
 
         constructor(target: eui.UiType) {
