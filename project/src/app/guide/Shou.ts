5eb6a18f7c9fd2e009c33fb4ab8e11c59142c5ee
wybosys <wybosys@gmail.com>
2018-11-29 19:55:06 +0800

    guide

--- project/src/app/guide/Shou.ts
+++ project/src/app/guide/Shou.ts
@@ -13,14 +13,66 @@ module app.guide {
         idrRight: eui.LabelU;
         idrTop: eui.LabelU;
         mcShou: eui.MovieClipU;
+
         //skin }
 
         constructor(target: eui.UiType) {
             super();
+            this.clickedToClose = true;
+            this.onlyFiltersTouchEnabled = true;
+            this.queue = manager.guide.queue;
+            this._target = target;
+            this.addFilter(this._target);
         }
 
-        //data:model.Guide;
+        data: data.Guide;
         direction: nn.Direction;
         private _target: eui.UiType;
+
+        onLoaded() {
+            super.onLoaded();
+            this.mcShou.play();
+            this.updateData();
+        }
+
+        updateData() {
+            super.updateData();
+
+            if (this.direction)
+                this.currentState = nn.DirectionToString(this.direction);
+            else
+                this.currentState = 'none';
+
+            let idr: eui.LabelU;
+            switch (this.direction) {
+                case nn.Direction.UP:
+                    idr = this.idrTop;
+                    break;
+                case nn.Direction.DOWN:
+                    idr = this.idrBottom;
+                    break;
+                case nn.Direction.LEFT:
+                    idr = this.idrLeft;
+                    break;
+                case nn.Direction.RIGHT:
+                    idr = this.idrRight;
+                    break;
+            }
+
+            if (idr) {
+                idr.visible = true;
+                idr.text = this.data.desc;
+            }
+        }
+
+        updateLayout() {
+            super.updateLayout();
+            this.updateFilters();
+        }
+
+        bestPosition(): nn.Point {
+            let tcenter = eui.ConvertPoint(this._target, nn.getBounds(this._target).center, null);
+            return tcenter;
+        }
     }
 }
--- project/src/app/wgt/EntryButton.ts
+++ project/src/app/wgt/EntryButton.ts
@@ -20,7 +20,16 @@ module app.wgt {
             br.color = nn.Color.Random();
             this.spBkg.brush = br;
 
-            this.signals.connect(nn.SignalClicked, this._actOpenEntry, this);
+            // 如果绑定了entryid
+            if (this._dentry) {
+
+                // 处理模块打开
+                this.signals.connect(nn.SignalClicked, this._actOpenEntry, this);
+
+                // 处理新手
+                manager.guide.signals.connect(kSignalShowGuide, this._updateGuide, this);
+                this._updateGuide();
+            }
         }
 
         // 实体标记，用来自动打开
@@ -35,17 +44,64 @@ module app.wgt {
         }
 
         // 用来显示标记的方向，现在给新手导引用
-        public idrDirection: string = null;
-
-        // 用来控制标记的偏移
-        public idrOffset: string = null;
+        public guideDirection: string = null;
 
         private _actOpenEntry() {
             if (!this._dentry)
                 return;
 
+            // 关闭新手
+            this.clearGuide();
+
             // 跳转到模块
             this._dentry.open();
+        }
+
+        // 当前的新手数据
+        _guide: data.Guide;
+
+        clearGuide() {
+            if (this._guide) {
+                manager.guide.setReaded(this._guide);
+                this._guide = null;
+            }
+        }
+
+        // 当现实隐藏时，更新新手
+        onVisibleChanged() {
+            super.onVisibleChanged();
+            if (this._guide)
+                this._showGuide();
+        }
+
+        // 更新新手数据
+        protected _updateGuide() {
+            let guide = manager.guide.findGuide(this.entryIdr);
+            if (guide == this._guide)
+                return;
+            this._guide = guide;
+            this._showGuide();
+        }
+
+        // 显示新手
+        protected _showGuide() {
+            // 当前没有新手数据
+            if (!this._guide) {
+                return;
+            }
+
+            // 如果当前的没有显示在舞台上，则不显示新手
+            if (nn.isAppearing(this) == false)
+                return;
+
+            // 如果当前打开的desktop不是控件的直接父级，则需要关闭所有的弹出窗口
+            if (nn.Desktop.Current() != eui.DesktopU.FromView(this))
+                nn.Desktop.CloseAllOpenings();
+
+            let dlg = new guide.Shou(this);
+            dlg.data = this._guide;
+            dlg.direction = nn.DirectionFromString(this.guideDirection);
+            dlg.open(true);
         }
     }
 }
