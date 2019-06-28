6e50b0ddb8ec7defcf82610d39fab8e82e165171
wybosys <wybosys@gmail.com>
2019-06-28 12:18:17 +0800

    夸张测试演示效果

--- project/src/app/wgt/Effects.ts
+++ project/src/app/wgt/Effects.ts
@@ -3,6 +3,9 @@ module app {
     export class TextShadow extends nn.ShadowEffect {
         constructor() {
             super();
+            this.distance = 10;
+            this.x = 30;
+            this.y = 30;
             this.color = nn.Color.Random();
         }
     }
